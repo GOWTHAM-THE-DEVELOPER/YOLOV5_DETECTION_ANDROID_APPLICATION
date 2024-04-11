d91258c1d16db35eed1944eff74bb1956b07d4b1
Gurumoorthy <gurumoorthyelumalai08@gmail.com>
2024-04-11 17:25:50 +0530

    ..

---
+++ app/src/main/java/com/programminghut/yolo_deploy/AppCompatActivity.java
@@ -0,0 +1,17 @@
+package com.programminghut.yolo_deploy;
+
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    }
+
+    protected ContentResolver getContentResolver() {
+        return null;
+    }
+}
